c170cfae6db685873dd043a02cf7e14c107f8ee3
HungWei Lin <boyabc123@berkeley.edu>
2014-10-13 12:41:10 -0700

    1st_trial

--- quiz/quiz1-l4/quiz1.r
+++ quiz/quiz1-l4/quiz1.r
@@ -1,13 +1,13 @@
 # Please load in the dataset included in the quiz1-l4 directory. It will be
 # required to perform the following tasks. The dataset includes data for
 # a family.
-
+load("family.rda")
 
 # calculate the mean and median age in the family. Store these as the
 # variables <mean.age> and <med.age> respectively.
 
-# mean.age <- your code here
-# med.age <- your code here
+mean.age <- mean(family$age)
+med.age <- median(family$age)
 
 
 # For each person in the dataset, calculate the squared difference between its
@@ -15,7 +15,7 @@
 # <height.diffs>. Note that this should be a numeric vector with length equal to
 # the number of observations in the dataset
 
-# height.diffs <- your code here
+height.diffs <- sapply(family$height, function(x) (x-mean(family$height))^2)
 
 
 
@@ -23,14 +23,14 @@
 # 1) people whose age is strictly greater than <mean.age>:  <ppl.old>
 # 2) people whose age is less than or equal to <mean.age>: <ppl.young>
 
-# ppl.old <- your code here
-# ppl.young <- your code here
+ppl.old <- family[family$age > mean(family$age),]
+ppl.young <- family[family$age <= mean(family$age),]
 
 # For each of your subsets, create a vector giving the weight of each person. Name
 # these variables <ppl.old.weight> and <ppl.young.weight>.
 
-# ppl.old.weight <- your code here
-# ppl.young.weight <- your code here
+ppl.old.weight <- ppl.old[,colnames(family) %in% c("firstName","weight")]
+ppl.young.weight <- ppl.young[,colnames(family) %in% c("firstName","weight")]
 
 
 # Please implement the function bmiByheight. Your function should take the
@@ -47,7 +47,8 @@
 
 bmiByheight <- function(height.range, height, bmi) {
   
-  # your code here
+  new_height_index=which(height.range[1]<=height & height<=height.range[2])
+  mean(bmi[new_height_index])
   
 }
 
@@ -58,3 +59,5 @@ bmiByheight <- function(height.range, height, bmi) {
 # 2) axis labels: "BMI" and "Age (Year)"
 # 3) a blue line with intercept= 22.89050 and slope= 0.03501
 
+plot(x = family$age,y = family$bmi, main="BMI vs Age", xlab="Age (Year)", ylab= "BMI")
+abline(a = 22.89050, b = 0.03501, col="BLUE")
